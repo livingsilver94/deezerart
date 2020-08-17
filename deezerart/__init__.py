b89f0e731289b4c088dc024ff68cf9adabffce90
Fabio Forni <fabio@redaril.me>
2020-08-18 00:54:57 +0200

    Add __version__

--- deezerart/__init__.py
+++ deezerart/__init__.py
@@ -5,3 +5,5 @@ PLUGIN_VERSION = '1.0.0'
 PLUGIN_API_VERSIONS = ['2.2']
 PLUGIN_LICENSE = "GPL-3.0-or-later"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-3.0.html"
+
+__version__ = PLUGIN_VERSION
