c7c65df4456fa99bba0ede8877c933c1f49fa200
Fabio Forni <fabio@redaril.me>
2020-08-18 13:36:21 +0200

    Add Artist tests

--- tests/test_deezer_object.py
+++ tests/test_deezer_object.py
@@ -1,8 +1,8 @@
 import json
 import unittest
 
-from parameterized import parameterized
 from deezerart.deezer import object as deezerobj
+from parameterized import parameterized
 
 
 class TestJSONParsing(unittest.TestCase):
@@ -13,10 +13,25 @@ class TestJSONParsing(unittest.TestCase):
             deezerobj.Album(title='Good Things', cover='https://api.deezer.com/album/54302092/image')
         ),
         (
-            'with_none',
+            'with_a_none',
             '{"title": "Good Things"}',
             deezerobj.Album(title='Good Things', cover=None)
         )
     ])
     def test_album(self, _name, json_doc, expected):
         self.assertEqual(json.loads(json_doc, object_hook=deezerobj.Album.from_json), expected)
+
+    @parameterized.expand([
+        (
+            'all_fields',
+            '{"name": "Metallica"}',
+            deezerobj.Artist(name='Metallica')
+        ),
+        (
+            'with_a_none',
+            '{}',
+            deezerobj.Artist(name=None)
+        )
+    ])
+    def test_artist(self, _name, json_doc, expected):
+        self.assertEqual(json.loads(json_doc, object_hook=deezerobj.Artist.from_json), expected)
