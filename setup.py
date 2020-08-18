06e62728b828c1c2f3e1cf4a69821870f4c7bc7b
Fabio Forni <fabio@redaril.me>
2020-08-18 11:34:25 +0200

    Set deps in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import find_packages, setup
 
 import deezerart
 
+deps = open('requirements.txt').readlines()
+test_deps = open('requirements.tests.txt').readlines()
+
 setup(
     name=deezerart.__name__,
     version=deezerart.__version__,
@@ -9,4 +12,6 @@ setup(
     packages=find_packages(),
 
     python_requires='>=3.5',
+    install_requires=deps,
+    tests_require=test_deps,
 )
