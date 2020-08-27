be2f050e59fe07dd436a3c1abffef3dc1101e248
Fabio Forni <fabio@redaril.me>
2020-08-27 23:36:50 +0200

    Add untested implementation of queue_images()

--- deezerart/deezerart.py
+++ deezerart/deezerart.py
@@ -6,8 +6,15 @@ PLUGIN_API_VERSIONS = ['2.2']
 PLUGIN_LICENSE = "GPL-3.0-or-later"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-3.0.html"
 
+
+from typing import List, Optional
+
+from picard import log
 from picard.coverart import providers
-from deezerart.deezer import Client
+from picard.coverart.image import CoverArtImage
+from PyQt5.QtNetwork import QNetworkReply
+
+from deezerart.deezer import Client, SearchOptions, obj
 
 
 class Provider(providers.CoverArtProvider):
@@ -16,7 +23,25 @@ class Provider(providers.CoverArtProvider):
         self.client = Client(self.album.tagger.webservice)
 
     def queue_images(self):
-        pass
+        search_opts = SearchOptions(title=self.metadata['artist'], album=self.metadata['musicbrainz_releasegroupid'])
+        self.client.advanced_search(search_opts, self._search_callback)
+        return self.WAIT
+
+    def _search_callback(self, results: Optional[List[obj.Object]], error: Optional[QNetworkReply.NetworkError]):
+        try:
+            if error or (results is None or len(results) == 0):
+                log.error("Deezerart: could not fetch search results: %s", error or "empty list")
+                return
+            for result in results:
+                if not isinstance(result, obj.Track):
+                    continue
+                album = self.metadata['musicbrainz_releasegroupid']
+                artist = self.metadata['artist']
+                if result.artist.name != artist and result.album.title != album:
+                    continue
+                self.queue_put(CoverArtImage(result.album.cover_url(obj.CoverSize.BIG)))
+        finally:
+            self.next_in_queue()
 
 
 providers.register_cover_art_provider(Provider)
