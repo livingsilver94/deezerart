49c3af9c58c7ae92ea03262874b32d1ce76e943c
Fabio Forni <fabio@redaril.me>
2020-08-29 13:57:45 +0200

    Add command to create deezerart.zip

--- setup.py
+++ setup.py
@@ -1,12 +1,33 @@
-from setuptools import find_packages, setup
+import os
+import shutil
+
+from setuptools import Command, find_packages, setup
 
 import deezerart
 
 deps = open('requirements.txt').readlines()
 test_deps = open('requirements.tests.txt').readlines()
 
+
+class MkpluginCommand(Command):
+    description = 'create a deezerart.zip plugin file'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for dirname, subdirs, _files in os.walk('deezerart'):
+            if '__pycache__' in subdirs:
+                shutil.rmtree(os.path.join(dirname, '__pycache__'))
+        shutil.make_archive('deezerart', 'zip', root_dir='deezerart', base_dir='.')
+
+
 setup(
-    name=deezerart.__name__,
+    name='deezerart',
     version=deezerart.__version__,
 
     packages=find_packages(),
@@ -14,4 +35,8 @@ setup(
     python_requires='>=3.5',
     install_requires=deps,
     tests_require=test_deps,
+
+    cmdclass={
+        "mkplugin": MkpluginCommand,
+    },
 )
