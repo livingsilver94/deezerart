6a0126dfef748f12685ced90556f056d46ee1e0f
Fabio Forni <fabio@redaril.me>
2020-08-29 15:31:29 +0200

    Use the project structure Picard expects

--- deezerart/__init__.py
+++ deezerart/__init__.py
@@ -1,3 +1,15 @@
-from .deezerart import PLUGIN_VERSION
+PLUGIN_NAME = "Deezer cover art"
+PLUGIN_AUTHOR = "Fabio Forni <livingsilver94>"
+PLUGIN_DESCRIPTION = "Fetch cover arts from Deezer"
+PLUGIN_VERSION = '1.0.0'
+PLUGIN_API_VERSIONS = ['2.2', '2.3', '2.4']
+PLUGIN_LICENSE = "GPL-3.0-or-later"
+PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-3.0.html"
+
+from picard import providers
+
+from .provider import Provider
 
 __version__ = PLUGIN_VERSION
+
+providers.register_cover_art_provider(Provider)
--- deezerart/provider.py
+++ deezerart/provider.py
@@ -1,11 +1,3 @@
-PLUGIN_NAME = "Deezer cover art"
-PLUGIN_AUTHOR = "Fabio Forni <livingsilver94>"
-PLUGIN_DESCRIPTION = "Fetch cover arts from Deezer"
-PLUGIN_VERSION = '1.0.0'
-PLUGIN_API_VERSIONS = ['2.2']
-PLUGIN_LICENSE = "GPL-3.0-or-later"
-PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-3.0.html"
-
 
 from typing import List, Optional
 
@@ -18,6 +10,8 @@ from .deezer import Client, SearchOptions, obj
 
 
 class Provider(providers.CoverArtProvider):
+    NAME = 'Deezer'
+
     def __init__(self, coverart):
         super().__init__(coverart)
         self.client = Client(self.album.tagger.webservice)
@@ -42,6 +36,3 @@ class Provider(providers.CoverArtProvider):
                 self.queue_put(CoverArtImage(result.album.cover_url(obj.CoverSize.BIG)))
         finally:
             self.next_in_queue()
-
-
-providers.register_cover_art_provider(Provider)
